14e213c3940c07a3bfd377d707af10262b80ca8d
vcinent <vincent@hienha.com>
2020-04-11 11:14:48 +0800

    add enemy module(big_ememy, mid_enemy, small_enemy)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pygame
 
 import myplane
 # import bullet
-# import enemy
+import enemy
 # import supply
 
 from pygame.locals import *
@@ -59,11 +59,45 @@ me_down_sound = pygame.mixer.Sound("sound/me_down.wav")
 me_down_sound.set_volume(0.2)
 
 
+def add_small_enemies(group1, group2, nums):
+    for i in range(nums):
+        es = enemy.SmallEnemy(bg_size)
+        group1.add(es)
+        group2.add(es)
+
+def add_mid_enemies(group1, group2, nums):
+    for i in range(nums):
+        em = enemy.MidEnemy(bg_size)
+        group1.add(em)
+        group2.add(em)
+
+def add_big_enemies(group1, group2, nums):
+    for i in range(nums):
+        eb = enemy.BigEnemy(bg_size)
+        group1.add(eb)
+        group2.add(eb)
+
+
 def main():
     pygame.mixer.music.play(-1)
 
     # 生成我方飞机
     mp = myplane.MyPlane(bg_size)
+
+    # 生成敌方飞机
+    enemies = pygame.sprite.Group()
+
+    # 生成敌方小型飞机
+    small_enemies = pygame.sprite.Group()
+    add_small_enemies(small_enemies, enemies, 15)
+
+    # 生成敌方中型飞机
+    mid_enemies = pygame.sprite.Group()
+    add_mid_enemies(mid_enemies, enemies, 4)
+
+    # 生成敌方大型飞机
+    big_enemies = pygame.sprite.Group()
+    add_big_enemies(big_enemies, enemies, 2)
 
     clock = pygame.time.Clock()
 
@@ -92,8 +126,30 @@ def main():
             mp.moveLeft()
         if key_pressed[K_d] or key_pressed[K_RIGHT]:
             mp.moveRight()
-        
+
         screen.blit(background, (0,0))
+
+        # 绘制大型敌机
+        for each in big_enemies:
+            each.move()
+            if switch_image:
+                screen.blit(each.image1, each.rect)
+            else:
+                screen.blit(each.image2, each.rect)
+
+            # 距离屏幕50像素，播放大型敌机音效
+            if each.rect.bottom > -50:
+                enemy3_fly_sound.play()
+
+        # 绘制中型敌机
+        for each in mid_enemies:
+            each.move()
+            screen.blit(each.image, each.rect)
+
+        # 绘制小型敌机
+        for each in small_enemies:
+            each.move()
+            screen.blit(each.image, each.rect)
 
         # 绘制我方飞机
         switch_image = not switch_image
